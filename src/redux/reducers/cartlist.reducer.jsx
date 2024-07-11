32c3ec7fcbd4852762100a8f37e393eb36a4c8d4
shahnazamiraslanova <bd7d396lw@code.edu.az>
2024-07-11 08:36:28 -0700

    Dəyişikliklərin təsviri

--- src/redux/reducers/cartlist.reducer.jsx
+++ src/redux/reducers/cartlist.reducer.jsx
@@ -1,20 +1,27 @@
-export const cartListReducer=(state =[],action)=>{
+export const cartListReducer=(state =JSON.parse(localStorage.getItem("cart")) || [],action)=>{
 switch (action.type) {
     case "ADD_TO_CART":
         let foundProduct=state.find(item=>item.id===action.payload.id)
 if(!foundProduct){
-    
+    localStorage.setItem("cart",JSON.stringify([...state,{...action.payload,count:1}]))
     return [...state,{...action.payload,count:1}]
 }else{
     foundProduct.count++;
+    localStorage.setItem("cart",JSON.stringify(state))
+
     return [...state]
 }
 
 case "COUNT_DECREMENT":
 if (action.payload.count>1) {
     action.payload.count--
+    localStorage.setItem("cart",JSON.stringify(state))
+
 }else{
-   return state.filter(item => item.id !==action.payload.id)
+   state= state.filter(item => item.id !==action.payload.id)
+   localStorage.setItem("cart",JSON.stringify(state))
+
+   return  state
 }
     return [...state]
         
